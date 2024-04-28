40116545296e93e6c3c11dbef5b956f62f417d15
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-04-28 22:58:39 +0100

    update

--- src/page/ScorePage.jsx
+++ src/page/ScorePage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function ScorePage() {
   const [matchData, setMatchData] = useState([]);
@@ -54,7 +55,7 @@ function ScorePage() {
             <div className='flex flex-col w-full md:w-[60%] m-auto justify-center'>
               <div className='flex justify-between items-center mb-2'>
                 <p className='font-bold '>{convertToNigeriaTimeWithDay(match.date)}</p>
-                <a className=' uppercase font-bold' href="./stats">Statistics</a>
+                <Link to="./stats">Statistics</Link>
               </div>
               <div className='flex  text-gray-400 mb-1 font-medium item justify-between'>
                 <p className='mr-7'>{match.status.type.description }</p>
